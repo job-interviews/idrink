274e1e78a8a884b3fd5c149c91f4cbda8210324d
Georgi Mirchev <gmirchev90@gmail.com>
2016-08-11 14:11:42 +0300

    Removed unused api

--- app/src/main/java/com/nmp90/idrink/mvp/bars/BarsPresenter.java
+++ app/src/main/java/com/nmp90/idrink/mvp/bars/BarsPresenter.java
@@ -5,21 +5,18 @@ import com.nmp90.idrink.mvp.BasePresenter;
 
 import javax.inject.Inject;
 
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-
 /**
  * Created by joro on 05.08.16.
  */
 
 public class BarsPresenter extends BasePresenter<BarsContract.View> implements BarsContract.Presenter {
 
-    private Api tourMeetApi;
+    private Api iDrinkApi;
     private BarsContract.View view;
 
     @Inject
-    public BarsPresenter(Api tourMeetApi, BarsContract.View view) {
-        this.tourMeetApi = tourMeetApi;
+    public BarsPresenter(Api iDrinkApi, BarsContract.View view) {
+        this.iDrinkApi = iDrinkApi;
         this.view = view;
     }
 
@@ -30,11 +27,6 @@ public class BarsPresenter extends BasePresenter<BarsContract.View> implements B
 
     @Override
     public void loadMeetings() {
-        tourMeetApi.getMeetings("").subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
-                .subscribe(meetings -> {
-                        view.displayBars(meetings);
-                }, err -> {
 
-                });
     }
 }
