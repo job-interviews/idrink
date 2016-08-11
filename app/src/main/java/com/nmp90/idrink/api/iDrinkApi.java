2447ff59bae4eb213a2c2be9b4cb8b5543f93e6e
Georgi Mirchev <gmirchev90@gmail.com>
2016-08-11 17:24:59 +0300

    Logger for retrofit

--- app/src/main/java/com/nmp90/idrink/api/iDrinkApi.java
+++ app/src/main/java/com/nmp90/idrink/api/iDrinkApi.java
@@ -2,6 +2,8 @@ package com.nmp90.idrink.api;
 
 import com.nmp90.idrink.utils.Constants;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -14,10 +16,16 @@ public class iDrinkApi {
     private iDrinkApi() {}
 
     public static Api createGettiApiService() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);  // <-- this is the important line!
+
         Retrofit.Builder builder = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(JacksonConverterFactory.create())
-                .baseUrl(Constants.MAPS_URL);
+                .baseUrl(Constants.MAPS_URL)
+                .client(httpClient.build());
 
         return builder.build().create(Api.class);
     }
---
+++ app/src/main/java/com/nmp90/idrink/api/models/BarsResult.java
@@ -0,0 +1,8 @@
+package com.nmp90.idrink.api.models;
+
+/**
+ * Created by joro on 11.08.16.
+ */
+
+public class BarsResult {
+}
